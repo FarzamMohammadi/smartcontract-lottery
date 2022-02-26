abbbb54dee342dc652d32ad0b44a582837116586
Farzam Mohammadi <72108716+FarzamMohammadi@users.noreply.github.com>
2022-02-26 00:50:39 -0500

    integration test created

--- scripts/helpful_scripts.py
+++ scripts/helpful_scripts.py
@@ -79,13 +79,11 @@ def deploy_mocks(decimals=DECIMALS, initial_value=INITIAL_VALUE):
 
 def fund_with_link(
     contract_address, account=None, link_token=None, amount=100000000000000000
-):  # 0.1 Link
+):  # 0.1 LINK
     account = account if account else get_account()
     link_token = link_token if link_token else get_contract("link_token")
-    # Without using the interface
     tx = link_token.transfer(contract_address, amount, {"from": account})
-    # Using the interface
-    # link_token_contract = interface.LinkTokenInterface(link_token.addres)
+    # link_token_contract = interface.LinkTokenInterface(link_token.address)
     # tx = link_token_contract.transfer(contract_address, amount, {"from": account})
     tx.wait(1)
     print("Fund contract!")
